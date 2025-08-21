4f26bdd32568a2ec12957b0021600352ecef2073
shibin <shibin>
2025-08-21 17:06:25 +0530

    deploy change

--- src/config/AxiosConfig.tsx
+++ src/config/AxiosConfig.tsx
@@ -1,6 +1,5 @@
 import axios from "axios";
-console.log(import.meta.env.VITE_BACKED_URL)
-const API = axios.create(
-    {baseURL: `${import.meta.env.VITE_BACKED_URL}/api`});
+
+const API = axios.create({baseURL: "https://voicedrop-backend.onrender.com/api"});
 
 export default API;
