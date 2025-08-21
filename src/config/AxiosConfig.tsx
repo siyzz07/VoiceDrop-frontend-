b56a84422cf06aa8ed121fe9c2ecb1a6bdd41dd0
shibin <shibin>
2025-08-21 16:12:20 +0530

    refactored code

--- src/config/AxiosConfig.tsx
+++ src/config/AxiosConfig.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
-
-const API = axios.create({baseURL: `${import.meta.env.VITE_BACKED_URL}`});
+console.log(import.meta.env.VITE_BACKED_URL)
+const API = axios.create(
+    {baseURL: `${import.meta.env.VITE_BACKED_URL}/api`});
 
 export default API;
