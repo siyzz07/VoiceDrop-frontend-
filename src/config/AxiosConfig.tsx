631b54d4be1bf17a32eba68e9133ec63283134d0
shibin <shibin>
2025-08-21 16:27:02 +0530

    deploy changes

--- src/config/AxiosConfig.tsx
+++ src/config/AxiosConfig.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 console.log(import.meta.env.VITE_BACKED_URL)
 const API = axios.create(
-    {baseURL: `${import.meta.env.VITE_BACKED_URL}/api`});
+    {baseURL: `${import.meta.env.VITE_BACKED_URL}`});
 
 export default API;
