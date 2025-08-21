75c1cec0a76be15665b68ca936497e10aaaddcee
shibin <shibin>
2025-08-21 09:39:54 +0530

    diploy chinges

--- src/config/AxiosConfig.tsx
+++ src/config/AxiosConfig.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "https://voicedrop-backend.onrender.com"});
+const API = axios.create({baseURL: "https://voicedrop-backend.onrender.com/api"});
 
 export default API;
