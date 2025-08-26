ff63ae43494d44af3c017b7bf0faa43142563e4a
shibin <shibin>
2025-08-26 13:51:48 +0530

    route bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import UserRoutes from "./routes/UserRoutes";
 
 const App = () => {
