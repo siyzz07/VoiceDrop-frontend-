d88aa0f872bc74f8c62b1685fb7a5b49a29507ab
shibin <shibin>
2025-12-01 00:55:47 +0530

    soket edit

--- src/config/Socket.tsx
+++ src/config/Socket.tsx
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Initialize Socket.IO
-// const socket = io("https://voicedrop-backend.onrender.com", { autoConnect: true });
-const socket = io("http://localhost:7001", { autoConnect: true });
+const socket = io("https://voicedrop-backend.onrender.com", { autoConnect: true });
+// const socket = io("http://localhost:7001", { autoConnect: true });
 
 export default socket;
